6b758d4f34371d2b13aed851bb01afdde136f61d
Tyreese2070 <tyreese.green1@outlook.com>
2026-03-07 12:28:07 +0000

    Update DB path for azure

--- smart_recipe/settings.py
+++ smart_recipe/settings.py
@@ -80,11 +80,15 @@ WSGI_APPLICATION = 'smart_recipe.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+if 'WEBSITE_HOSTNAME' in os.environ:
+    DB_PATH = '/home/db.sqlite3'
+else:
+    DB_PATH = BASE_DIR / 'db.sqlite3'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_PATH,
     }
 }
 
